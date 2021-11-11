e71d45b5f0c51fcea231a01613427d0713eb68b9
miv-dev <i@mike39.ru>
2021-11-11 19:37:31 +0200

    Update the shoplist in real time

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
             Log.d("MainActivity", "onCreate: $it")
+            if (count == 0){
+                val item = it[0]
+                viewModel.deleteShopItem(item)
+                count++
+            }
         }
-        viewModel.getShopList()
+
     }
 }
