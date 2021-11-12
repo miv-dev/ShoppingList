bbbf6660b5f7c4acfd0418136970992c78dbbdfa
miv-dev <i@mike39.ru>
2021-11-12 11:14:54 +0200

    Add recycler view

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,15 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl:ShopListRepository {
-    private val shopList = mutableListOf<ShopItem>()
     private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private val shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10){
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000){
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglist.presentation
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+
+class ShopItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+    val tvName: TextView = view.findViewById(R.id.tv_name)
+    val tvCount: TextView = view.findViewById(R.id.tv_count)
+
+}
